990d80e0e23f2860e22e67c47e44f1e7fe7130e4
NathanMumo <Nathanmumo37@gmail.com>
2017-06-21 10:07:36 +0300

    comments on js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
+// add animation to the page loader text
 $('.txt').html(function(i, html) {
   var chars = $.trim(html).split("");
 
   return '<span>' + chars.join('</span><span>') + '</span>';
 });
+// set a timeout function for the page loader
 var timeout;
 
 function myFunction() {
   timeout = setTimeout(showPage, 2000);
 }
-
+// show page when the page loader timeouts
 function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
