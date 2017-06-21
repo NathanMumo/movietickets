332281785aa0e2d84913c98d93d7426010ddab8c
NathanMumo <Nathanmumo37@gmail.com>
2017-06-21 11:25:31 +0300

    homepage carousel updates

--- js/script.js
+++ js/script.js
@@ -1,17 +1,33 @@
-// add animation to the page loader text
-$('.txt').html(function(i, html) {
-  var chars = $.trim(html).split("");
+function favmovie() {
+  var movielist = document.getElementById("movielist");
+  document.getElementById("mo").value = movielist.options[movielist.selectedIndex].text;
+}
 
-  return '<span>' + chars.join('</span><span>') + '</span>';
-});
-// set a timeout function for the page loader
+function timeRange() {
+  var timelist = document.getElementById("timelist");
+  document.getElementById("ti").value = timelist.options[timelist.selectedIndex].text;
+}
+
+function ageRange() {
+  var agelist = document.getElementById("agelist");
+  document.getElementById("ag").value = agelist.options[agelist.selectedIndex].text;
+}
 var timeout;
 
 function myFunction() {
-  timeout = setTimeout(showPage, 2000);
+  timeout = setTimeout(showPage, 1000);
 }
-// show page when the page loader timeouts
+
 function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
+}
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
