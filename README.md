9172d0609527dbce58d5213f2397f56470ea960b
ChristineWasike <christinewasike22@gmail.com>
2017-06-21 15:15:13 +0300

    MODIFIED FILES modified fies

---
+++ README.md
@@ -0,0 +1,22 @@
+# PIG DICE
+# Author's Name:
+ Christine Wasike
+# Project's Purpose:
+A web application that takes a number from a user and returns a range of numbers from 1 to the chosen number
+
+## Specs Sheet
+   1. Performs the ping pong function on integers.
+   2. Counts up to the provided number.
+   3. If the integer is divisible by 3 the output changes to "Ping".
+   4. If the integer is divisible by 5 the output changes to "Pong".
+   5. If the integer is divisible by both 3 and 5 the output changes to "Ping Pong".
+
+## LANGUAGE SETUP/ INSTALLATION
+   1. HTML
+   2. CSS
+   3. JavaScript
+   4. jQuery and Bootstrap libraries
+
+
+## License and Copyright
+[MIT license](license)
---
+++ js/script.js
@@ -0,0 +1,33 @@
+function favmovie() {
+  var movielist = document.getElementById("movielist");
+  document.getElementById("mo").value = movielist.options[movielist.selectedIndex].text;
+}
+
+function timeRange() {
+  var timelist = document.getElementById("timelist");
+  document.getElementById("ti").value = timelist.options[timelist.selectedIndex].text;
+}
+
+function ageRange() {
+  var agelist = document.getElementById("agelist");
+  document.getElementById("ag").value = agelist.options[agelist.selectedIndex].text;
+}
+var timeout;
+
+function myFunction() {
+  timeout = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
