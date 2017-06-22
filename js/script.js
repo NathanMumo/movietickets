2c205142be0d2dea0e10de83a30c31255d6ee70d
NathanMumo <Nathanmumo37@gmail.com>
2017-06-22 13:42:19 +0300

    news updates

--- js/script.js
+++ js/script.js
@@ -15,23 +15,55 @@ function ageRange() {
 var timeout;
 
 function myFunction() {
-  timeout = setTimeout(showPage, 100);
+  timeout = setTimeout(showPage, 4500);
 }
 
 function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
 }
-// Get the modal
-var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-$(document).ready(function() {
-
-})
+$(document).ready(function () {
+  $('#woman, #eyes').on('click', function(e){
+    e.preventDefault();
+    var target= $(this).get(0).id == 'woman' ? $('#eyes') : $('#woman');
+    $('html, body').stop().animate({
+      scrollTop: target.offset().top
+    }, 1000);
+  });
+  $('#guardian, #guard').on('click', function(e){
+    e.preventDefault();
+    var target= $(this).get(0).id == 'guardian' ? $('#guard') : $('#guardian');
+    $('html, body').stop().animate({
+      scrollTop: target.offset().top
+    }, 1000);
+  });
+  $('#henry, #bookie').on('click', function(e){
+    e.preventDefault();
+    var target= $(this).get(0).id == 'henry' ? $('#bookie') : $('#henry');
+    $('html, body').stop().animate({
+      scrollTop: target.offset().top
+    }, 1000);
+  });
+  $('#night, #even').on('click', function(e){
+    e.preventDefault();
+    var target= $(this).get(0).id == 'night' ? $('#even') : $('#night');
+    $('html, body').stop().animate({
+      scrollTop: target.offset().top
+    }, 1000);
+  });
+  $('#carz, #cars').on('click', function(e){
+    e.preventDefault();
+    var target= $(this).get(0).id == 'carz' ? $('#cars') : $('#carz');
+    $('html, body').stop().animate({
+      scrollTop: target.offset().top
+    }, 1000);
+  });
+  $('#spider, #spidermanhc').on('click', function(e){
+    e.preventDefault();
+    var target= $(this).get(0).id == 'spider' ? $('#spidermanhc') : $('#spider');
+    $('html, body').stop().animate({
+      scrollTop: target.offset().top
+    }, 1000);
+  });
+});
